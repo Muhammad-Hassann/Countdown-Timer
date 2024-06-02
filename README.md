aa68862d3a158ad2e64033e7fb25a8cd0690929f
Muhammad Hassan <muhammadhassan202005@gmail.com>
2024-06-03 02:02:24 +0500

    Adding CountDown Timer App

---
+++ README.md
@@ -0,0 +1,19 @@
+# Enhanced Countdown Timer
+
+* This is a user-friendly countdown timer built with TypeScript. It leverages Inquirer for interactive input from the user and Chalk for visually appealing output messages.
+
+## Features:
+
+* Intuitive Interface: Enter the desired countdown duration in seconds with ease.
+* Clear Output: Get time updates with a visually appealing format during the countdown.
+* Customizable Experience: (Optional) Explore customization options in the future, such as specifying different units (minutes, hours) or defining a callback function for actions upon completion.
+
+## Technology Stack:
+
+* TypeScript: Ensures code reliability through type checking and prevents runtime errors.
+* Inquirer: Facilitates user interaction for a seamless experience.
+* Chalk: Enhances the user interface with stylish text formatting.
+
+## Additional Considerations:
+
+* Error Handling: Implement validation to ensure the user enters a valid time value (positive integer).
---
+++ index.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+console.log(chalk.bold.magenta("\n\t######################################"));
+console.log(chalk.bold.blue("\n\t\t ⏱  Countdown Timer ⏱ \n"));
+console.log(chalk.bold.magenta("\t######################################"));
+const userInput = await inquirer.prompt({
+    name: "time",
+    message: "Enter time in seconds",
+    type: "input"
+});
+if (isNaN(userInput.time)) {
+    console.log(`Please enter a valid number`);
+}
+else if (userInput.time < 0) {
+    console.log("You can't have negative time");
+}
+else {
+    showCountdown(userInput.time);
+}
+function showCountdown(initialNum) {
+    let currentNum = initialNum;
+    let interval = setInterval(() => {
+        if (currentNum > 0) {
+            console.log(`00 : ${currentNum.toString().padStart(2, "0")}`);
+            currentNum--;
+        }
+        else {
+            clearInterval(interval);
+            console.log(chalk.bold.blue("\n\ ⌛  Countdown Finished! ⌛"));
+        }
+    }, 1000);
+}
